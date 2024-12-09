4001a81d8d0742e1673c8af7ed3b9e7404458717
Rafael <rafaelpaniago1@gmail.com>
2024-12-09 17:47:21 -0300

    Novas consultas sql feitas

--- df_consultas.py
+++ df_consultas.py
@@ -3,10 +3,32 @@ import sqlite3
 
 conn = sqlite3.connect('db_df_base.db')
 
-consulta01 = """
-    select * from table_df_base
-    limit 12
+
+# CONSULTA 01 -> 10 PRODUTOS MAIS VENDIDOS
+
+# consulta01 = """
+#     select descricao, sum(quantidade) as total_vendido
+#     from table_df_base
+#     group by descricao
+#     order by total_vendido DESC
+#     limit 10
+# """
+
+# con01_df = pd.read_sql_query(consulta01, conn)
+# print(con01_df)
+
+
+#----------------------------------------------------
+
+# CONSULTA 02 -> PAÍS COM MAIOR NÚMERO DE CLIENTES
+
+consulta02 = """
+    select nome_pais, count(distinct id_cliente) as total_clientes_unicos
+    from table_df_base
+    group by nome_pais
+    order by total_clientes_unicos desc
+    limit 10
 """
 
-con01_df = pd.read_sql_query(consulta01, conn)
-print(con01_df)
+con02_df = pd.read_sql_query(consulta02, conn)
+print(con02_df)
